ab6a25293bd460a0aae63f586082cc4cb51e1ade
BrandonC123 <brandoc318@gmail.com>
2022-11-05 16:36:41 -0700

    Update url and added preliminary styling with MUI.

--- pages/treasuryDisplay.tsx
+++ pages/treasuryDisplay.tsx
@@ -1,3 +1,4 @@
+import { Paper } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -5,22 +6,30 @@ type displayProps = {
     safeAddress: string;
 };
 
-// API docs: https://safe-transaction-mainnet.safe.global/   
-const API_BASE_URL = "safe-transaction-mainnet.safe.global/api";
-
 function TreasuryDisplay({ safeAddress }: displayProps) {
+    const apiPlaceholder = "";
+    const API_URL = `https://api-goerli.etherscan.io/api?module=account&action=balance&address=${safeAddress}&tag=latest&apikey=${apiPlaceholder}`;
     const [balance, setBalance] = useState("");
     useEffect(() => {
         getBalance();
     }, []);
     async function getBalance() {
-        const resp = await axios.get(
-            `${API_BASE_URL}/v1/safes/${safeAddress}/balances/`
-        );
+        const resp = await axios.get(API_URL);
         setBalance(resp.data.balance);
     }
-    // Temporarily store in div
-    return <div>{balance}</div>;
+    return (
+        <Paper
+            elevation={3}
+            style={{
+                fontSize: "1.75rem",
+                width: "20vh",
+                textAlign: "center",
+                margin: ".5em auto",
+            }}
+        >
+            {balance}$112.5
+        </Paper>
+    );
 }
 
 export default TreasuryDisplay;
