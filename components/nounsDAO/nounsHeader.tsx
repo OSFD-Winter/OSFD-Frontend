11cfc9a35b8fe877eab714afd3457a3262ae29c5
BrandonC123 <brandoc318@gmail.com>
2022-11-08 17:39:01 -0800

    Change to background image and style title text.

--- components/nounsDAO/nounsHeader.tsx
+++ components/nounsDAO/nounsHeader.tsx
@@ -1,15 +1,28 @@
 function NounsHeader() {
     return (
-        <div>
-            <img
+        <div
+            style={{
+                background: `url("nouns-header.png") center center no-repeat`,
+                backgroundSize: "cover",
+                height: "100vh",
+            }}
+        >
+            <h1
                 style={{
-                    height: "100vh",
-                    width: "100%",
+                    position: "absolute",
+                    color: "white",
+                    top: "50%",
+                    left: "8%",
+                    transform: "translateY(-50%)",
+                    fontSize: "4rem",
+                    fontWeight: "bold",
+                    margin: "0"
                 }}
-                src={"nouns-header.png"}
-                width="100%"
-                alt=""
-            />
+            >
+                Create, <br /> Discover
+                <br /> and Join
+                <br /> <i style={{ fontWeight: 400 }}>Amazing</i> DAOs
+            </h1>
         </div>
     );
 }
