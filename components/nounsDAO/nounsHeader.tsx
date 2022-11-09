4c0736aab05a8ee2a834297005631d1c450e6a7f
BrandonC123 <brandoc318@gmail.com>
2022-11-08 16:40:09 -0800

    Initialize component

---
+++ components/nounsDAO/nounsHeader.tsx
@@ -0,0 +1,17 @@
+function NounsHeader() {
+    return (
+        <div>
+            <img
+                style={{
+                    height: "100vh",
+                    width: "100%",
+                }}
+                src={"nouns-header.png"}
+                width="100%"
+                alt=""
+            />
+        </div>
+    );
+}
+
+export default NounsHeader;
