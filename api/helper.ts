27ed21b939337cf97613c960b8c06c3088b56b9f
daniilkuz <you@example.com>
2022-11-11 04:42:33 +0600

    created a folder for api, created helper.ts and interfaces.ts

---
+++ api/helper.ts
@@ -0,0 +1,55 @@
+import type { ProposalState } from "./interfaces";
+
+export const getVotes = (
+  space: string,
+  proposalsState: ProposalState
+): string => {
+  return `query Proposals {
+    proposals(
+      first: 20,
+      skip: 0,
+      where: {
+        space_in: ["${space}"],
+        state: "${proposalsState}"
+      },
+      orderBy: "created",
+      orderDirection: desc
+    ) {
+      id
+      title
+      body
+      choices
+      start
+      end
+      snapshot
+      state
+      author
+      space {
+        id
+        name
+      }
+    }
+  }`;
+};
+
+export const getProposals = async (
+  space: string,
+  proposalsState: ProposalState,
+  source: string
+): Promise<any> => {
+  try {
+    const response = await fetch(`${source}`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        query: getVotes(space, proposalsState),
+      }),
+    });
+    const data = await response.json();
+    // console.log(await data.json());
+    // console.log("data ", data);
+    return data.data.proposals;
+  } catch (e) {
+    console.log(e);
+  }
+};
