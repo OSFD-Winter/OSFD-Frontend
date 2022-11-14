2b73116b45d330a8b3d848a61d598962f059547f
Akshar Mehta <aksharmehta@Akshars-MacBook-Air.local>
2022-11-14 18:09:44 +0530

    Added a new Header and replaced the image

--- pages/header.js
+++ pages/header.js
@@ -1,18 +1,18 @@
-import React from "react";
-import Logo from "../assets/osfd-big-logo.png";
-import Image from "next/Image";
-import Diamond from "../assets/diamond.png";
+import React from 'react';
+import Logo from '../assets/osfd-big-logo.png';
+import Image from 'next/image';
+import Diamond from '../assets/diamond.png';
 const header = () => {
-  return (
-    <div className="akshar__header">
-      <div className="akshar__logo">
-        <Image src={Logo} alt="" />
-      </div>
-      <div className="akshar__diamond">
-        <Image height="180px" width="100px" src={Diamond} alt="" />
-      </div>
-    </div>
-  );
+    return (
+        <div className="akshar__header">
+            <div className="akshar__logo">
+                <Image src={Logo} alt="" />
+            </div>
+            <div className="akshar__diamond">
+                <Image height="180px" width="100px" src={Diamond} alt="" />
+            </div>
+        </div>
+    );
 };
 
 export default header;
