9bf53a060e252697e6e897cfae26f8aa41404e47
BrandonC123 <brandoc318@gmail.com>
2022-11-14 17:46:48 -0800

    Refactor footer css to stick to bottom and fix centering.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,23 +1,61 @@
 // @ts-nocheck
 
-
 function Footer() {
     return (
-        <div style={
-            {width: "100%"}
-        }>
-            <div style={{ backgroundImage: "linear-gradient(to right, #01010A, #010529, #171e74, #2673FF)", position: "relative", minHeight: 170, color: "white", }}>
-                <div style={{ display: "flex", justifyContent:"center", paddingTop: 70, maxWidth:"100%" }}>
-                <div  style={{position: "relative", top: 35, fontSize: 17, left:110, width: "100%", marginRight: 50}}>  
-                    <a href={"/terms"} style={{}}>  Terms of Service </a> | Sales Agreements | Privacy Policy
-                    </div>
-                    <div style={{ width: "100%", position: "relative", bottom: 10, paddingLeft: 125, right: 10}}> <img src="./osfd-small-logo.png" alt="osfd"></img> </div>
-                    
-                    <div style={{position: "relative", top: 20, fontSize: 19, width:"40%", paddingBottom: 40, top: 35, right: 50}}>© 2022 OSFD All rights reserved</div>
+        <footer
+            style={{
+                backgroundImage:
+                    "linear-gradient(to right, #01010A, #010529, #171e74, #2673FF)",
+                position: "relative",
+                height: "150px",
+                color: "white",
+                width: "100vw",
+            }}
+        >
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    position: "relative",
+                    height: "100%",
+                    maxWidth: "90%",
+                    paddingTop: "30px",
+                    margin: "0 auto",
+                }}
+            >
+                <div
+                    style={{
+                        fontSize: 17,
+                    }}
+                >
+                    <a href={"/terms"}> Terms of Service </a> | Sales Agreements
+                    | Privacy Policy
+                </div>
+                <div
+                    style={{
+                        position: "absolute",
+                        top: "50%",
+                        transform: "translateY(-50%)",
+                        display: "flex",
+                        justifyContent: "center",
+                        width: "100%",
+                    }}
+                >
+                    {" "}
+                    <img src="./osfd-small-logo.png" alt="osfd"></img>{" "}
+                </div>
+
+                <div
+                    style={{
+                        fontSize: 19,
+                    }}
+                >
+                    © 2022 OSFD All rights reserved
                 </div>
             </div>
-        </div>
-    )
+        </footer>
+    );
 }
 
-export default Footer
+export default Footer;
