de9c3c0dc853788defd2af42ee3292fafa78a807
jeefxM <87044530+jeefxM@users.noreply.github.com>
2022-11-16 16:20:29 +0400

    i added referral and refactored footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,14 +6,14 @@ function Footer() {
         <div style={
             {width: "100%"}
         }>
-            <div style={{ backgroundImage: "linear-gradient(to right, #01010A, #010529, #171e74, #2673FF)", position: "relative", minHeight: 170, color: "white", }}>
+            <div style={{ backgroundImage: "linear-gradient(to right, #01010A, #010529, #171e74, #2673FF)", position: "relative", minHeight: 180, color: "white", }}>
                 <div style={{ display: "flex", justifyContent:"center", paddingTop: 70, maxWidth:"100%" }}>
-                <div  style={{position: "relative", top: 35, fontSize: 17, left:110, width: "100%", marginRight: 50}}>  
+                <div  style={{position: "relative", top: 35, fontSize: ("calc(12px + 0.5vw)"), left:"4vw", width: "100%", marginRight: "3vw"}}>  
                     <a href={"/terms"} style={{}}>  Terms of Service </a> | Sales Agreements | Privacy Policy
                     </div>
-                    <div style={{ width: "100%", position: "relative", bottom: 10, paddingLeft: 125, right: 10}}> <img src="./osfd-small-logo.png" alt="osfd"></img> </div>
+                    <div style={{ width: "100%", position: "relative", bottom: "-0.2vw", paddingLeft: "5vw", right: "0.5vw"}}> <img src="./osfd-small-logo.png" alt="osfd" style={{width:"calc(85px + 3vw)"}}></img> </div>
                     
-                    <div style={{position: "relative", top: 20, fontSize: 19, width:"40%", paddingBottom: 40, top: 35, right: 50}}>© 2022 OSFD All rights reserved</div>
+                    <div style={{position: "relative", top: 20, fontSize: ("calc(9px + 0.5vw)"), width:"40%", paddingBottom: "3vw", top: 35, right: "2vw"}}>© 2022 OSFD All rights reserved</div>
                 </div>
             </div>
         </div>
--- components/referral.tsx
+++ components/referral.tsx
@@ -1,52 +1,291 @@
-import { TextField } from "@mui/material"
-import Button from "@mui/material/Button"
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import InputAdornment from '@mui/material/InputAdornment';
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import InputAdornment from "@mui/material/InputAdornment";
+import { FaCopy } from "react-icons/fa";
 
+function Refferal() {
+  return (
+    <div
+      className="referralProgram"
+      style={{
+        backgroundImage:
+          "radial-gradient(circle at 44.6% 35.17%, #9f25a6 0, #9018a1 25%, #80059c 50%, #6f0097 75%, #5e0092 100%)",
+        height: 750,
 
-
-function Referral(){
-    return(
-        <div className="referralProgram" style={{backgroundImage: "radial-gradient(circle at 44.6% 35.17%, #9f25a6 0, #9018a1 25%, #80059c 50%, #6f0097 75%, #5e0092 100%)",minHeight: 800, borderRadius: 50, width: "85%", position: "relative", left: "5%", bottom: 50, }}>
-          <div className="textImage" style={{position: "relative", width:"100%", top: 60, maxHeight: 180, paddingRight: 100 }}>
-          <a style={{position: "relative", left:"25%", maxHeight:"auto", maxWidth:"200px", outline:"none"}}> <img src="glasses.png" style={{minWidth: "5.255vw", maxWidth: "25.233vw"}}></img></a>
-          <strong style={{position: "relative",fontSize: "3.3vw", color: "white", left: "27%", bottom: 25}}>Referral Program</strong>
-          </div>
-
-        
-          <div className="text1" style={{display: "flex", justifyContent: "center", marginTop: 125, marginRight: 140}}>
-            <span style={{position:"relative", bottom: "4%", marginLeft: 40, color: "white", fontSize: "1.25vw", fontWeight: "bold",}}>Enter Wallet REFERRAL LINK</span>
-            <TextField sx={{width: 420, marginLeft: 7, position: "relative", backgroundColor: "white", borderRadius: 2, height: 1, fontSize: "1.2vw"}} id="outlined-basic"  size="small" variant="outlined" placeholder="yourEmail@gmail.com"  />
-            <Button variant="outlined" size="large" style={{backgroundColor: "white"}} sx={{backgroundColor: "white", marginLeft: 5, width: "5.78vw", fontSize:"0.8vw", borderRadius: 2, position: "relative", bottom: 1.5}}>SEND</Button>
-            
-          </div>
-
-        
-          <div className="text2" style={{display: "flex", justifyContent: "center", marginTop: 25, marginRight: 300}}>
-            <strong style={{position:"relative", bottom: "4%", right: 9, marginLeft: 47, color: "white", fontSize: "1.25vw", fontWeight: "bold", paddingRight: 62.5}}>Your REFERRAL LINK</strong> 
-            <TextField defaultValue={"https://www.teamnouns.xyz?ref=ref=0xC"} sx={{width: 420, marginLeft: 10, position: "relative", backgroundColor: "white", borderRadius: 2, height: 1, fontSize: "10vw"}} id="outlined-basic"  size="small"  InputProps={{readOnly: true, endAdornment:(<InputAdornment position="start"><ContentCopyIcon /></InputAdornment>)}}  />
-          
-          </div>
-  
-          <div style={{display: "flex", justifyContent: "center", height: "1vw", paddingTop: 80, marginRight: "8.9vw", paddingBottom: 175}}>
-              <span style={{fontSize: "1.25vw", fontWeight: "bold", color: "white", marginRight: 88}}>Current Reward total for each <br></br>member joining through link</span>
-              <span style={{fontSize: "2.7vw", fontWeight: "bold", color: "white", marginLeft: 5}}>$671.24</span>
-          </div>
-  
-          <div style={{position:"relative" ,bottom: -6, width:"100%", display:"flex", justifyContent:"space-between"}}>
-            <a style={{position: "relative", left:85}}><img src="group 263.png" style={{minWidth: "5.255vw", maxWidth: "15.233vw"}}></img></a>
-            <a style={{position:"relative", left: 230,}}><img src="noun-205.png" style={{minWidth: "5.255vw", maxWidth: "15.233vw"}}></img></a>
-            <a style={{position:"relative", marginRight: 25, top: 2, left: 100}}><img src="noun-105.png" style={{minWidth: "5.255vw", maxWidth: "15.233vw"}}></img></a>
-            <a><img src="noun-346.png" style={{minWidth: "5.255vw", maxWidth: "15.233vw", position:"relative", left: 150}}></img></a>
-            <a style={{position:"relative", marginRight: 75, top: 17}}><img src="noun-227.png" style={{minWidth: "5.255vw", maxWidth: "15.233vw"}}></img></a>
-            
-          </div>
-  
+        borderRadius: 50,
+        width: "cal(85vw + 10vw)",
+        position: "relative",
+        margin: 0,
+        marginTop: 100,
+        boxSizing: "border-box",
+        maxWidth: "85%",
+        marginLeft: "7.5%",
+        marginBottom: 100
+      }}
+    >
+      <div
+        className="textImage"
+        style={{
+          maxWidth: "100%",
+          color: "white",
+          minWidth: "2rem",
+          height: "auto",
+          textAlign: "center",
+          padding: "10px",
+          paddingTop: "50px",
+        }}
+      >
+        {" "}
+        <a style={{ width: "500px" }}>
+          {" "}
+          <img
+            src="Group 276.png"
+            style={{
+              width: "13%",
+              minWidth: "135px",
+              position: "relative",
+              top: "1vw",
+            }}
+          ></img>{" "}
+        </a>
+        <span
+          style={{
+            fontSize: "calc(25px + 2vw)",
+            position: "relative",
+            marginLeft: "1.5vw",
+            paddingRight: "0px",
+            fontWeight: "bold",
+          }}
+        >
+          {" "}
+          Referral Program
+        </span>
       </div>
-    )
+
+      <div
+        className="Refferal Wallet link"
+        style={{
+          paddingTop: 50,
+          textAlign: "center",
+          display: "flex",
+          width: "60%",
+          marginLeft: "17.7%",
+          justifyContent: "space-around",
+          // position: "relative",
+          // left: "4vw",
+        }}
+      >
+        <span
+          style={{
+            color: "white",
+            width: "calc(50rem + 1vw)",
+            fontSize: "calc(8px + 1vw)",
+            paddingRight: 10,
+            fontWeight: "bold",
+            position: "relative",
+            top: "0.2vw",
+            right: "0.8vw",
+          }}
+        >
+          Enter WAllet REFERRAL LINK
+        </span>
+        <TextField
+          variant="outlined"
+          size="small"
+          placeholder="youremail@gmail.com"
+          style={{
+            width: "800px",
+            minWidth: 100,
+            color: "white",
+            borderRadius: 5,
+            backgroundColor: "white",
+            height: 40,
+            position: "relative",
+            top: "0.2vw",
+            // backgroundColor: "white",
+          }}
+        ></TextField>
+
+        <Button
+          variant="outlined"
+          size="medium"
+          style={{
+            backgroundColor: "white",
+            marginLeft: "3vw",
+            minWidth: "45px",
+            position: "relative",
+            top: 1,
+            right: "1vw",
+            width: "13vw",
+            fontSize: "calc(10px + 0.3vw)",
+            height: "calc(31px + 0.7vw)",
+            borderRadius: 9,
+            color: "#0F155A",
+          }}
+          sx={{ fontSize: "16 + 1vw", fontWeight: "bold" }}
+        >
+          {" "}
+          SEND
+        </Button>
+      </div>
+
+      <div
+        style={{
+          textAlign: "center",
+          display: "flex",
+          width: "60%",
+          marginLeft: "5.44%",
+          paddingTop: 20,
+          justifyContent: "space-around",
+        }}
+      >
+        <span
+          style={{
+            color: "white",
+            width: "calc(50rem + 1vw)",
+            fontSize: "calc(8px + 1vw)",
+            fontWeight: "bold",
+            position: "relative",
+            top: "0.2vw",
+            left: "1.33vw",
+          }}
+        >
+          Your REFERRAL Link
+        </span>
+        <TextField
+          defaultValue={"https://www.teamnouns.xyz?ref=ref=0xC"}
+          
+          variant="outlined"
+          size="small"
+          InputProps={{
+            readOnly: true,
+            endAdornment: (
+              <InputAdornment position="end">
+                <FaCopy color="#0F155A" />
+              </InputAdornment>
+            ),
+          }}
+          style={{
+            width: "527.5px",
+            minWidth: 90,
+            maxWidth: "550px",
+            color: "white",
+            position: "relative",
+            left: "0.22vw",
+            borderRadius: 5,
+            height: 40
+
+            // backgroundColor: "white",
+          }}
+          sx={{ backgroundColor: "white" }}
+        ></TextField>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: "1vw",
+          paddingTop: 80,
+          marginRight: "10%",
+          paddingBottom: 0,
+        }}
+      >
+        <span
+          style={{
+            fontSize: "calc(8px + 1vw)",
+            fontWeight: "bold",
+            color: "white",
+            marginRight: "5vw",
+            height: "50px",
+            position: "relative",
+            left: "0.9vw",
+          }}
+        >
+          Current Reward total for each <br></br>member joining through link
+        </span>
+        <span
+          style={{
+            fontSize: "calc(20px + 2vw)",
+            fontWeight: "bold",
+            color: "white",
+            marginRight: 10,
+          }}
+        >
+          $671.24
+        </span>
+      </div>
+
+      <div
+        className="Images"
+        style={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          height: "auto",
+        }}
+      >
+        <a style={{}}>
+          <img
+            src="group 263.png"
+            style={{
+              // width: "127px",
+              // maxheight: "108px",
+              maxWidth: "calc(17px + 8.5vw)",
+              position: "relative",
+              left: "4vw",
+              bottom: "-0.3vw",
+            }}
+          ></img>
+        </a>
+
+        <a style={{}}>
+          <img
+            src="noun-205.png"
+            style={{
+              maxWidth: "calc(25px + 8.5vw)",
+              position: "absolute",
+              left: "27vw",
+              bottom: 0,
+            }}
+          ></img>
+        </a>
+        <a style={{}}>
+          <img
+            src="noun-105.png"
+            style={{
+              maxWidth: "calc(17px + 8.5vw)",
+              position: "absolute",
+              bottom: 0,
+              left: "40vw",
+            }}
+          ></img>
+        </a>
+        <a>
+          <img
+            src="noun-346.png"
+            style={{
+              maxWidth: "calc(20px + 9vw)",
+              position: "absolute",
+              bottom: 0,
+              right: "15vw",
+            }}
+          ></img>
+        </a>
+        <a style={{}}>
+          <img
+            src="noun-227.png"
+            style={{
+              maxWidth: "calc(19px + 7vw)",
+              position: "absolute",
+              right: "5vw",
+              bottom: 0
+            }}
+          ></img>
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Referral
-
-
+export default Refferal;
 
