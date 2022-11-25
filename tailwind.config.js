f7d7bdb1efe27f86e82cffacd08e265d20994802
Akshar Mehta <aksharmehta@Akshars-MacBook-Air.local>
2022-11-25 16:21:41 +0530

    Responsive header and footer using tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "22vw": "22vw",
+        "8vw": "8vw",
+      },
+    },
+    colors: {
+      // ...
+      blue: {
+        1000: "#060124",
+        50: "#265cf0",
+      },
+      white: "white",
+    },
   },
   plugins: [],
 };
