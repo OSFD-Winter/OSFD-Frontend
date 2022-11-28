a511ec99cecf0de6dfb1c7d09c2261b87ff775f7
Stephen Chau <chtzch0713@gmail.com>
2022-11-28 01:35:00 -0800

    wallet balance in global state, auto check list

--- pages/getting-started.tsx
+++ pages/getting-started.tsx
@@ -1,9 +1,16 @@
 // @ts-nocheck
 import { useEffect, useState, useRef } from "react";
+import { ReducerContextProvider, useReducerContext } from "../api/context";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import axios from "axios";
 
 function GettingStarted() {
+  const { state, dispatch } = useReducerContext();
+  const [contractAddr, setContractAddr] = useState(
+    "0x09aD6Fb74584fFbA72C65419c03741325CAE00a1" // TeamNouns
+  );
+
   const ref1 = useRef();
   const ref2 = useRef();
   const ref3 = useRef();
@@ -78,6 +85,60 @@ function GettingStarted() {
     }
   }, [checked6]);
 
+  useEffect(() => {
+    if (window !== undefined) {
+      const { ethereum } = window;
+
+      // Check if user has metamask
+      const checkMetamaskAvailability = async () => {
+        if (!ethereum) {
+          return;
+        }
+        setChecked1(true);
+        checkNetwork();
+      };
+
+      // Check if user has switched to goeril network
+      const checkNetwork = async () => {
+        if (window.ethereum.networkVersion != 5) {
+          return;
+        }
+        setChecked2(true);
+        checkBalance();
+      };
+
+      // Check if balance is >= 0.05 eth
+      const checkBalance = async () => {
+        console.log(state);
+        if ((state.walletBalance == "") | (parseFloat(state.walletBalance) <= 0.05)) {
+          return;
+        }
+        setChecked3(true);
+        checkNFTOwnership();
+      };
+
+      // Check if he has an nft of team nouns
+      const checkNFTOwnership = () => {
+        axios
+          .get(
+            `https://testnets-api.opensea.io/api/v1/assets?owner=${state.walletAddress}&asset_contract_address=${contractAddr}&order_direction=desc&offset=0&limit=20&include_orders=false`
+          )
+          .then((res) => {
+            if (res.data.assets.length == 0) {
+              return;
+            }
+            // Holds an asset under the contract address
+            setChecked4(true);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+
+      checkMetamaskAvailability();
+    }
+  }, []);
+
   const Checkbox = ({ label, value, onChange }) => {
     return (
       <label>
@@ -114,44 +175,29 @@ function GettingStarted() {
               <Checkbox value={checked1} onChange={handleChange1} />
             </p>
             <p className="text-xl break-words my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
+              It is necessary to have your own cryptocurrency wallet in order to interact with the
+              Etherium blockchain.
             </p>
             <p className="text-lg break-words my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
+              Go to <a href="https://metamask.io/download/">https://metamask.io/download/</a> to
+              download and install Metamask to your device.
             </p>
           </div>
         </div>
         <div ref={ref2} className="text-white font-bold mt-8 mx-32 my-5">
           <div className="container flex flex-col">
             <p className="text-4xl my-5 break-words">
-              Switch network to goeril testnet
+              Switch network to Goeril testnet
               <Checkbox value={checked2} onChange={handleChange2} />
             </p>
             <p className="text-xl break-words my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
+              In order to test all OSFD's features and functionality, you will need to obtain test
+              Ether used in the Goerli network. To get Goerli test ETH, you will first switch to the
+              Goeril network.
             </p>
             <p className="text-lg break-words my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
+              To switch to the Goeril network, simply open your Metamask wallet and select the
+              Goerli network in the network droplist.
             </p>
           </div>
         </div>
@@ -162,20 +208,12 @@ function GettingStarted() {
               <Checkbox value={checked3} onChange={handleChange3} />
             </p>
             <p className="text-xl break-words my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
+              You can not obtain free test Ether using your wallet address. Some faucets require
+              posting on your twitter/facebook account to prevent spamming.
             </p>
             <p className="text-lg break-words my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
+              Go to <a href="https://faucetlink.to/goerli">https://faucetlink.to/goerli</a> and
+              select any active faucet to obtain test Eth.
             </p>
           </div>
         </div>
@@ -185,21 +223,10 @@ function GettingStarted() {
               Mint NFT from team nouns dao
               <Checkbox value={checked4} onChange={handleChange4} />
             </p>
-            <p className="text-xl break-words my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
-            </p>
+            <p className="text-xl break-words my-2">You are now ready to mint your first NFT.</p>
             <p className="text-lg break-words my-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-              dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollit anim id est laborum.
+              Go to the OSFD main page. Chose any NFT offerings and click on the mint button. You
+              will complete the transaction via Metamask.
             </p>
           </div>
         </div>
