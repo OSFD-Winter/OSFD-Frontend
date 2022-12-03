6d27d35491ae3657049b57d9566e35d0e935fe32
BrandonC123 <brandoc318@gmail.com>
2022-12-03 00:02:46 -0800

    Clean up code for typescript.

--- components/metamaskButton.tsx
+++ components/metamaskButton.tsx
@@ -1,50 +1,30 @@
 import { useEffect, useState } from "react";
-import { Button, ButtonUnstyled } from "@mui/material";
-import { useWeb3React } from "@web3-react/core";
+import { Button } from "@mui/material";
 import { useReducerContext } from "../api/context";
 import { ethers } from "ethers";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
 const MetaMaskButton = () => {
   //TODO: dont use ethers
   const [haveMetamask, sethaveMetamask] = useState(true);
   const [isConnected, setIsConnected] = useState(false);
   const { state, dispatch } = useReducerContext();
+  // const { ethereum } = typeof window !== "undefined" && window;
+  const ethereum = typeof window !== "undefined" && window ? window.ethereum : {};
 
-  const { ethereum } = typeof window !== "undefined" && window;
-  const { active, activate, account, library } = useWeb3React();
-  const changeNetWork = async () => {
-    try {
-      await ethereum.request({
-        method: "wallet_switchEthereumChain",
-        params: [{ chainId: "0x5" }], // Goerli Testnet
-      });
-      setIsConnected(true);
-    } catch (err: any) {
-      // This error code indicates that the chain has not been added to MetaMask.
-      if (err.code === 4902) {
-        try {
-          await ethereum.request({
-            method: "wallet_addEthereumChain",
-            params: [
-              {
-                chainId: "0x5",
-                chainName: "Goerli Testnet",
-                rpcUrls: ["https://goerli.prylabs.net"],
-              },
-            ],
-          });
-          setIsConnected(true);
-        } catch (addError) {
-          // handle "add" error
-          setIsConnected(false);
-        }
+  // See if Metamask is installed on browser
+  useEffect(() => {
+    if (window !== undefined) {
+      if (!ethereum) {
+        sethaveMetamask(false);
       }
-      // handle other "switch" errors
-      setIsConnected(false);
     }
-    // handle other "switch" errors
-    setIsConnected(false);
-  };
+  }, []);
 
   const connectWallet = async () => {
     try {
@@ -59,33 +39,58 @@ const MetaMaskButton = () => {
       let balance = await new ethers.providers.Web3Provider(window.ethereum).getBalance(
         accounts[0]
       );
+      // TODO:
       let bal = ethers.utils.formatEther(balance);
       dispatch({ type: "setWalletBalance", payload: bal });
-      //console.log(state);
     } catch (error) {
-      //console.log(error);
+      console.log(error);
       setIsConnected(false);
     }
   };
 
-  // See if Metamask is installed on browser
-  useEffect(() => {
-    if (window !== undefined) {
-      const { ethereum } = window;
-      const checkMetamaskAvailability = async () => {
-        if (!ethereum) {
-          sethaveMetamask(false);
-        }
-        sethaveMetamask(true);
-      };
-      checkMetamaskAvailability();
+  const changeNetWork = async () => {
+    try {
+      await ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x5" }], // Goerli Testnet
+      });
+      setIsConnected(true);
+    } catch (err: any) {
+      // This error code indicates that the chain has not been added to MetaMask.
+      if (err.code === 4902) {
+        addEthereumChain();
+      }
+      // handle other "switch" errors
+      setIsConnected(false);
     }
-  }, []);
+  };
+
+  async function addEthereumChain() {
+    try {
+      await ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            chainId: "0x5",
+            chainName: "Goerli Testnet",
+            rpcUrls: ["https://goerli.prylabs.net"],
+          },
+        ],
+      });
+      setIsConnected(true);
+    } catch (addError) {
+      // handle "add" error
+      setIsConnected(false);
+    }
+  }
+
   return (
     <Button
-      onClick={() => connectWallet}
+      onClick={() => {
+        connectWallet();
+      }}
       sx={{
-        // display: isConnected ? "None" : "flex",
+        display: isConnected ? "none" : "flex",
         position: "absolute",
         top: 10,
         right: 10,
@@ -101,20 +106,12 @@ const MetaMaskButton = () => {
         alignItems: "center",
       }}
     >
-      {/* haveMetaMask */}
-      {true ? (
+      {haveMetamask ? (
         <div>
-          {/* isConnected */}
-          {false ? (
+          {isConnected ? (
             <></>
           ) : (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-              }}
-            >
+            <div className="flex items-center">
               <div>Connect with Metamask</div>
               <img
                 style={{
