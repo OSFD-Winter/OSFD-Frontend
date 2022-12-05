0bd7da278873c2009f113d96357eb612dc73ced4
BrandonC123 <brandoc318@gmail.com>
2022-12-04 23:19:15 -0800

    Refactor overall code and update when button/account details is displayed.

--- components/metamaskButton.tsx
+++ components/metamaskButton.tsx
@@ -4,7 +4,6 @@ import { useReducerContext } from "../api/context";
 import { ethers } from "ethers";
 import { toast } from "react-toastify";
 import { injected } from "../src/web3ReactInjector";
-import { useWeb3React } from "@web3-react/core";
 
 declare global {
   interface Window {
@@ -16,17 +15,15 @@ const generalStyle = {
   position: "absolute",
   top: 10,
   right: 10,
-  width: "20vw",
   bgcolor: "#306ac7",
   borderRadius: "5px",
   color: "white !IMPORTANT",
   padding: "8px 24px",
-};
+} as const;
 
 const MetaMaskButton = () => {
   const [haveMetamask, sethaveMetamask] = useState(true);
   const [isConnected, setIsConnected] = useState(false);
-  const { activate } = useWeb3React();
   const { state, dispatch } = useReducerContext();
   const ethereum = typeof window !== "undefined" && window ? window.ethereum : {};
 
@@ -35,14 +32,13 @@ const MetaMaskButton = () => {
     if (window !== undefined) {
       if (!ethereum) {
         sethaveMetamask(false);
+        return;
       }
     }
-    // Check if wallet is connected already, if so hide button
+    // Check if wallet is connected already, if so hide button and initialize states
     injected.isAuthorized().then((authorized) => {
       if (authorized) {
-        console.log(authorized);
-        // activate(injected);
-        connectWallet();
+        initializeWalletAndBalance();
       }
     });
   }, []);
@@ -52,23 +48,24 @@ const MetaMaskButton = () => {
         sethaveMetamask(false);
         return;
       }
-      const accounts = await ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      dispatch({ type: "setWalletAddress", payload: accounts[0] });
-      changeNetWork();
-      const balance = await new ethers.providers.Web3Provider(window.ethereum).getBalance(
-        accounts[0]
-      );
-      const bal = +balance / +1000000000000000000;
-      dispatch({ type: "setWalletBalance", payload: bal.toString() });
-      // toast("Wallet Connected");
+      initializeWalletAndBalance();
     } catch (error) {
       console.log(error);
       setIsConnected(false);
     }
   };
-
+  async function initializeWalletAndBalance() {
+    const accounts = await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    dispatch({ type: "setWalletAddress", payload: accounts[0] });
+    changeNetWork();
+    const balance = await new ethers.providers.Web3Provider(window.ethereum).getBalance(
+      accounts[0]
+    );
+    const bal = +balance / +1000000000000000000;
+    dispatch({ type: "setWalletBalance", payload: bal.toString() });
+  }
   const changeNetWork = async () => {
     try {
       await ethereum.request({
@@ -76,6 +73,7 @@ const MetaMaskButton = () => {
         params: [{ chainId: "0x5" }], // Goerli Testnet
       });
       setIsConnected(true);
+      toast("Wallet Connected");
     } catch (err: any) {
       // This error code indicates that the chain has not been added to MetaMask.
       if (err.code === 4902) {
@@ -104,8 +102,8 @@ const MetaMaskButton = () => {
       setIsConnected(false);
     }
   }
-
-  return haveMetamask && !isConnected ? (
+  // Display button if metamask is installed or needs to be installed
+  return (haveMetamask && !isConnected) || (!haveMetamask && !isConnected) ? (
     <Button
       onClick={() => {
         connectWallet();
