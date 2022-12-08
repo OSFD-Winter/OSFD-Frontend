141b540e940fd3313beaa443bd3aa64e1bf62fe6
Saba Kobaidze <sbkobaidze@gmail.com>
2022-12-08 15:32:03 +0400

    created versions page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+    },
     extend: {
       spacing: {
         "22vw": "22vw",
