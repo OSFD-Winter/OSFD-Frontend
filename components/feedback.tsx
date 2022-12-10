40288b33bb070b3e0455143a578fe6728321fa64
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2022-12-09 21:37:02 -0500

    ux changes to feedback component

--- components/feedback.tsx
+++ components/feedback.tsx
@@ -24,7 +24,6 @@ function Feedback({ hash: any }) {
   // handle image upload
   const uploadImage = () => {
     if (image == null) return;
-    setUploading(true);
     const address = `images/${image.name + v4()}`;
     const imageRef = ref(storage, address);
     if (update === 1) {
@@ -37,6 +36,8 @@ function Feedback({ hash: any }) {
         setUploading(false);
       });
     }
+    setUploading(false);
+    setImage(null);
   };
 
   const pushToDiscord = () => {
@@ -50,7 +51,7 @@ function Feedback({ hash: any }) {
     const condensedData = "Email: " + email + "\nDescription: " + desc + "\nImage: " + url;
     const message = {
       content: condensedData,
-      username: "User Feedback || Title: " + title,
+      username: title,
     };
     if (title === "" || desc === "" || email === "") {
       setTimeout(() => {}, 4000);
@@ -67,132 +68,147 @@ function Feedback({ hash: any }) {
       console.log(error);
     }
     setSent(true);
+    toast.success("Feedback Received");
   };
 
+  useEffect(() => {
+    if (image) {
+      uploadImage();
+    }
+  }, [image]);
+
   return (
-    <div style={{ width: "100%", paddingInline: "20vw" }}>
-      <Paper
-        elevation={3}
-        style={{
-          paddingInline: "10vw",
-          backgroundColor: "#f8f8f8",
-          paddingBlock: 40,
-          color: "#01052a",
-        }}
-      >
-        {!sent && (
-          <>
-            <div
+    <>
+      <div className="">
+        {sent === false ? (
+          <div style={{ width: "100%", paddingInline: "20vw" }}>
+            <Paper
+              elevation={3}
               style={{
-                display: "flex",
-                justifyContent: "center",
+                paddingInline: "10vw",
+                backgroundColor: "#f8f8f8",
+                paddingBlock: 40,
                 color: "#01052a",
-                fontSize: 40,
               }}
             >
-              FEEDBACK
-            </div>
+              {!sent && (
+                <>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      color: "#01052a",
+                      fontSize: 40,
+                    }}
+                  >
+                    FEEDBACK
+                  </div>
 
-            <TextField
-              variant="standard"
-              label="Title"
-              value={title}
-              onChange={(event) => {
-                setTitle(event.target.value);
-              }}
-              style={{ padding: 10 }}
-              fullWidth
-            />
-            <br></br>
+                  <TextField
+                    required
+                    variant="standard"
+                    label="Title"
+                    value={title}
+                    onChange={(event) => {
+                      setTitle(event.target.value);
+                    }}
+                    style={{ padding: 10 }}
+                    fullWidth
+                  />
+                  <br></br>
 
-            <TextField
-              id="outlined-name"
-              label="Description"
-              variant="standard"
-              value={desc}
-              onChange={(event) => {
-                setDesc(event.target.value);
-              }}
-              style={{ padding: 10 }}
-              fullWidth
-              multiline
-              rows={3}
-            />
-            <br></br>
-            <TextField
-              id="outlined-name"
-              label="E-mail"
-              variant="standard"
-              value={email}
-              onChange={(event) => {
-                setEmail(event.target.value);
-              }}
-              style={{ padding: 10 }}
-              fullWidth
-              multiline
-              rows={1}
-            />
-            <br></br>
-            <div style={{ display: "flex", justifyContent: "left" }}>
-              {url === "NA" ? (
-                <Box
-                  style={{
-                    height: 100,
-                    display: "flex",
-                    justifyContent: "center",
-                    padding: 10,
-                    alignItems: "center",
+                  <TextField
+                    required
+                    id="outlined-name"
+                    label="Description"
+                    variant="standard"
+                    value={desc}
+                    onChange={(event) => {
+                      setDesc(event.target.value);
+                    }}
+                    style={{ padding: 10 }}
+                    fullWidth
+                    multiline
+                    rows={3}
+                  />
+                  <br></br>
+                  <TextField
+                    required
+                    id="outlined-name"
+                    label="E-mail"
+                    variant="standard"
+                    value={email}
+                    onChange={(event) => {
+                      setEmail(event.target.value);
+                    }}
+                    style={{ padding: 10 }}
+                    fullWidth
+                    multiline
+                    rows={1}
+                  />
+                  <br></br>
+                  <div style={{ display: "flex", justifyContent: "left" }}>
+                    {url === "NA" ? (
+                      <Box
+                        style={{
+                          height: 100,
+                          display: "flex",
+                          justifyContent: "center",
+                          padding: 10,
+                          alignItems: "center",
+                        }}
+                      >
+                        <Input
+                          type="file"
+                          name="file"
+                          id="input_img"
+                          accept="image/*"
+                          onChange={(event) => {
+                            setImage(event.target.files[0]);
+                            setUploading(true);
+                          }}
+                        />
+                        <Box sx={{ display: "flex", justifyContent: "center" }}>
+                          <Button type="submit" style={{ height: 35 }}>
+                            {uploading === false ? (
+                              ""
+                            ) : (
+                              <img
+                                src="https://logosbynick.com/wp-content/uploads/2021/01/animated-gif.gif"
+                                alt=""
+                                height="30px"
+                                width="30px"
+                              />
+                            )}
+                          </Button>
+                        </Box>
+                      </Box>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </>
+              )}
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  class="px-8 py-3 text-red bg-blue-600 rounded cursor-not-allowed focus:outline-none disabled:opacity-75"
+                  disabled={sent || email === "" || title === "" || desc === ""}
+                  // push to discord using webhooks
+                  onClick={() => {
+                    stopRender.current = false;
+                    pushToDiscord();
                   }}
                 >
-                  <Input
-                    type="file"
-                    name="file"
-                    id="input_img"
-                    accept="image/*"
-                    onChange={(event) => {
-                      setImage(event.target.files[0]);
-                    }}
-                  />
-                  <Box sx={{ display: "flex", justifyContent: "center" }}>
-                    <Button onClick={uploadImage} style={{ height: 35 }}>
-                      {uploading === false ? (
-                        "upload"
-                      ) : (
-                        <img
-                          src="https://logosbynick.com/wp-content/uploads/2021/01/animated-gif.gif"
-                          alt=""
-                          height="30px"
-                          width="30px"
-                        />
-                      )}
-                    </Button>
-                  </Box>
-                </Box>
-              ) : (
-                "Image Uploaded."
-              )}
-            </div>
-          </>
+                  {sent ? "" : "Send"}
+                </Button>
+              </div>
+            </Paper>
+          </div>
+        ) : (
+          ""
         )}
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Button
-            style={{
-              backgroundColor: "#1b2f91",
-              color: "white",
-              paddingInline: 40,
-            }}
-            disabled={sent || title === "" || desc === "" || email === ""}
-            // push to discord using webhooks
-            onClick={() => {
-              stopRender.current = false;
-              pushToDiscord();
-            }}
-          >
-            {sent ? "Received!" : "Send"}
-          </Button>
-        </div>
-      </Paper>
-    </div>
+      </div>
+    </>
   );
 }
 
