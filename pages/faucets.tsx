6ef9d5edef46b94d78abbef139c58e2222fbe20b
heronlancellot <heronlancellot@id.uff.br>
2022-12-12 16:59:39 -0300

    Create Header OSFD Faucet Page

--- pages/faucets.tsx
+++ pages/faucets.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Image from "next/image";
+
 import styles from "../styles/Faucets.module.css";
+import BigLogo from "../public/osfd-big-logo.png";
 
 const faucets = () => {
+  const Header = () => {
+    return (
+      <nav className={styles.header__container}>
+        <div className={styles.logo}>
+          <Image src={BigLogo} alt="" />
+        </div>
+      </nav>
+    );
+  };
+
   return (
     <>
+      <Header />
       <div className={styles.container}>
         <div>faucets</div>
       </div>
