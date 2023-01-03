4e714892d52a927bf55887b05c90614163bd98bb
heronlancellot <heronlancellot@id.uff.br>
2023-01-03 20:03:46 -0300

    Create Faucet Image Icons & Style

--- pages/faucets.tsx
+++ pages/faucets.tsx
@@ -3,31 +3,59 @@ import Image from "next/image";
 
 import styles from "../styles/Faucets.module.css";
 import BigLogo from "../public/osfd-big-logo.png";
+import {
+  alchemy,
+  allThatNode,
+  chainlink,
+  ethDrop,
+  paradigm,
+  pk910,
+  unitap,
+} from "../public/faucets components/faucetImages.js";
 
 const faucets = () => {
   const Header = () => {
     return (
       <nav className={styles.header__container}>
         <div className={styles.logo}>
-          <Image src={BigLogo} alt="" />
+          <Image src={BigLogo} alt="OSFD Logo" />
         </div>
       </nav>
+    );
+  };
+
+  const FaucetImage = () => {
+    return (
+      <>
+        <div className={styles.image__container}>
+          <Image src={alchemy} />
+          <Image src={allThatNode} />
+          <Image src={chainlink} />
+          <Image src={ethDrop} />
+          <Image src={paradigm} />
+          <Image src={pk910} />
+          <Image src={unitap} />
+        </div>
+      </>
     );
   };
 
   return (
     <>
       <Header />
+      <FaucetImage />
       <div className={styles.container}>
         <div className={styles.cards}>
           <div className={styles.card}>
-            <div className="image">imagem</div>
+            <div className="image">image</div>
 
             <div className="amount">amount</div>
 
             <div className="balance">balance</div>
 
             <div className="wallet">wallet</div>
+
+            <div className="visit">visit button</div>
           </div>
         </div>
       </div>
---
+++ public/faucets components/faucetImages.js
@@ -0,0 +1,7 @@
+export { default as alchemy } from './alchemy.png';
+export { default as allThatNode } from './all-that-node.png';
+export { default as chainlink } from './chainlink.png';
+export { default as ethDrop } from './eth-drop.png';
+export { default as paradigm } from './paradigm.png';
+export { default as pk910 } from './pk910.png';
+export { default as unitap } from './unitap.png';
